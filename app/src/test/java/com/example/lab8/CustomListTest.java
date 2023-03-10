048cdf6fb2fe77288060f99b365f0933e82b4b6d
“Harsh1207” <gosalia@ualberta.ca>
2023-03-10 14:42:44 -0700

    Red: Testiong Has function

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -36,14 +36,14 @@ public class CustomListTest {
 //        assertFalse(list.hasCity(city));
     }
 
-    @Test
-    public void testDelete() {
-        CustomList list = new CustomList(null, citylist);
-        City city = new City("Edmonton", "AB");
-        list.addCity(city);
-        list.delete(city);
-        assertFalse(list.hasCity(city));
-    }
+//    @Test
+//    public void testDelete() {
+//        CustomList list = new CustomList(null, citylist);
+//        City city = new City("Edmonton", "AB");
+//        list.addCity(city);
+//        list.delete(city);
+//        assertFalse(list.hasCity(city));
+//    }
 
 
 //        CustomList list = new CustomList(null, citylist);
